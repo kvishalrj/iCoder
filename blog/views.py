a8b9411660d8e86af52f55a780ed8ea0a9008848
Vishal Kumar <kvishalrj2020@gmail.com>
2023-11-24 09:35:31 +0530

    tinyMCE editor added

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,8 @@ def blogHome(request):
 
 def blogPost(request, slug):
     post = Post.objects.filter(slug=slug).first()
+    post.views = post.views + 1
+    post.save()
     comments = BlogComment.objects.filter(post=post, parent=None)
     replies = BlogComment.objects.filter(post=post).exclude(parent=None)
     replyDict = {}
