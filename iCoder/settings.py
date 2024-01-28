0a21106500cd9d699482bde575495649bab6e556
Vishal Kumar <kvishalrj2020@gmail.com>
2024-01-28 13:03:02 +0530

    database update

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 from django.contrib.messages import constants as messages
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,45 +77,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'iCoder.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASE_URL = config('DATABASE_URL')
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'iCoder',  # Database Name
-        'USER': 'postgres',  # PostgreSQL Username
-        'PASSWORD': 'postgre1234',  # PostgreSQL Password
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600) # type: ignore
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'djongo',
-#         'NAME': 'iCoder',
-#         'ENFORCE_SCHEMA': True,
-#         'CLIENT': {
-#             'host': 'mongodb+srv://kvikram2022:vkmongodb2024@icoder.ui2176v.mongodb.net/',
-#             'port': 27017,
-#             'username': 'kvikram2022',
-#             'password': 'vkmongodb2024'
-#         }
-#     }
-# }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
