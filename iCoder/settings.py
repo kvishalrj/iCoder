6f906b88c042074908f6e521702ccdf3ec1c92ee
Ravindra Pandey <ravindrapandey99999@gmail.com>
2024-01-28 20:40:41 +0530

    update

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -72,7 +72,8 @@ DATABASES = {
     )
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 AUTH_PASSWORD_VALIDATORS = [
     {
