3208d18c3f931045582aa9f93272dd6787e19722
Ravindra Pandey <ravindrapandey99999@gmail.com>
2024-01-28 20:31:47 +0530

    update

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,6 @@
+# build_files.sh
 pip install -r requirements.txt
-python3.11 manage.py collectstatic --no-input --clear
+
+# make migrations
+python3.9 manage.py migrate 
+python3.9 manage.py collectstatic
