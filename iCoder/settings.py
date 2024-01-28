64d5337784d9c42ab8973d1a69e3fbf9ce4d60a2
Vishal Kumar <kvishalrj2020@gmail.com>
2024-01-28 11:52:58 +0530

    database migration

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'iCoder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'iCoder',  # Database Name
+        'USER': 'postgres',  # PostgreSQL Username
+        'PASSWORD': 'postgre1234',  # PostgreSQL Password
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
