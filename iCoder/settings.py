b126c069ad076e2e5d590faefc3276223a0a5d3d
Vishal Kumar <kvishalrj2020@gmail.com>
2024-01-28 18:08:12 +0530

    commit

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ax+$sts$h(yvigl3&v=*ejw%l%in2bvkqvvz=rc%12s#+wpw5a'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
